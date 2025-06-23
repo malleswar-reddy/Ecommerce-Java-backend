754ee444c4eccdb749b8eebdecbc68c6e4aaedb2
Ramcharan-Goud <ramcharangoud8@gmail.com>
2025-06-23 10:47:13 +0530

    EC-100 Ecommerce project structure created

---
+++ shippingservice/src/main/java/com/ecommerce/shippingservice/ShippingserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.shippingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippingserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShippingserviceApplication.class, args);
+	}
+
+}
